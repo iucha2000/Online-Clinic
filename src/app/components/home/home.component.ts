ccf528415f6d18bb0247e8305f50c65c89f73b31
iuri.megreladze <iuri.megreladze@I-MEGRELADZE>
2024-11-06 16:59:38 +0400

    1. Add Categories table in HomeComponent 2. Add Category enum 3. Change html/css styles for HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Category } from '../../data/Category';
+
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  categories: Category[] = Object.values(Category);
+
+  ViewFullCategories(){
+    //TODO add scrolling
+    //TODO fix spacing when values dont fit
+    //TODO fix padding between count and name
+  }
 }
